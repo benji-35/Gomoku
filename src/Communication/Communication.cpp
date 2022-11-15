fde8961992cc90544f3744ba0ad6b312af839b12
benji-35 <benjamin.delvert@epitech.eu>
2022-11-15 16:04:29 +0100

    CHG : test listen lines

--- src/Communication/Communication.cpp
+++ src/Communication/Communication.cpp
@@ -7,10 +7,20 @@
 
 #include "Communication.hpp"
 
+#include <iostream>
+#include <string>
+
 Gomoku::Communication::Communication()
 {
 }
 
 Gomoku::Communication::~Communication()
 {
+}
+
+void Gomoku::Communication::listen() {
+    std::string line;
+    while (std::getline (std::cin, line)) {
+        std::cout << line << std::endl;
+    }
 }
--- src/Communication/Communication.hpp
+++ src/Communication/Communication.hpp
@@ -10,6 +10,8 @@ namespace Gomoku {
         public:
             Communication();
             ~Communication();
+
+            void listen();
     };
 }
 
